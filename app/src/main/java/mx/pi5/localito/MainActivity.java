69dba3176fa29a77ba8348ecb7c02ff542e93590
Maskeit <miguelagustin182@gmail.com>
2023-10-09 11:47:37 -0600

    feat:UI-Login-Register

--- app/src/main/java/mx/pi5/localito/MainActivity.java
+++ app/src/main/java/mx/pi5/localito/MainActivity.java
@@ -2,14 +2,20 @@ package mx.pi5.localito;
 
 import androidx.appcompat.app.AppCompatActivity;
 import mx.pi5.localito.layouts.HeaderView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent i;
+    Button to_login, to_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.buyer_sample);
-    //pendiente por codificar
+        setContentView(R.layout.anonym_login);
+
     }
 }
