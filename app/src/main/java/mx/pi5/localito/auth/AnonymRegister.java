3d6159faa222648859d720a7852a997051f0d065
Mauricio <mcardenas@ucol.mx>
2023-10-12 12:46:06 -0600

    fix: Import fix

--- app/src/main/java/mx/pi5/localito/auth/AnonymRegister.java
+++ app/src/main/java/mx/pi5/localito/auth/AnonymRegister.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 import mx.pi5.localito.R;
 import mx.pi5.localito.databinding.ActivityMainBinding;
-import  mx.pi5.localito.auth.anonym_register;
 
 public class AnonymRegister extends AppCompatActivity {
     ActivityMainBinding b;
