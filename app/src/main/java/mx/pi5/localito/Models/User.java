4bf6072af808c8bf3a9e501ba9b9d01d7b863231
Maskeit <miguelagustin182@gmail.com>
2023-10-14 20:33:32 -0600

    feat: I have added the new package and the User Model

---
+++ app/src/main/java/mx/pi5/localito/Models/User.java
@@ -0,0 +1,67 @@
+package mx.pi5.localito.Models;
+
+public class User {
+    private int id;
+    private String email;
+    private String password;
+    private String phone;
+    private String name;
+    private String last_name;
+
+    public User(int id, String email, String password, String phone, String name, String last_name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.name = name;
+        this.last_name = last_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
