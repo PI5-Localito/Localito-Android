25f23b3e161c9a31f97961c9d4b96fc22edb86ce
Nolazco <carlos_sceptile@hotmail.com>
2023-10-17 17:39:23 -0600

    feat: Product catalogue created
    Recycler view with products created

--- app/src/main/java/mx/pi5/localito/ProductCatalogue.java
+++ app/src/main/java/mx/pi5/localito/ProductCatalogue.java
@@ -1,14 +1,25 @@
 package mx.pi5.localito;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import mx.pi5.localito.databinding.ActivityProductCatalogueBinding;
 
 public class ProductCatalogue extends AppCompatActivity {
+    ActivityProductCatalogueBinding b;
+    String[] products = {"Coca", "Pepsi", "Cagada de perro", "Piedad de Dios"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_catalogue);
+        b = ActivityProductCatalogueBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
+
+        b.rvProducts.setLayoutManager(new LinearLayoutManager(this));
+        ProductAdapter adapter = new ProductAdapter(products);
+        b.rvProducts.setAdapter(adapter);
     }
 }
