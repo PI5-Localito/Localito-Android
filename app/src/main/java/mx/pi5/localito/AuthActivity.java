789c375417603cd928277c8961ce9fcdc22cac91
Mauricio <mcardenas@ucol.mx>
2023-10-18 07:52:07 -0600

    feat: Added searchview

--- app/src/main/java/mx/pi5/localito/AuthActivity.java
+++ app/src/main/java/mx/pi5/localito/AuthActivity.java
@@ -20,20 +20,5 @@ public class AuthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         b = ActivityAuthBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
-
-        b.btnIniciar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                usu = b.txtUsu.getText().toString();
-                pass = b.txtPass.getText().toString();
-
-                if(usu.isEmpty() || pass.isEmpty()){
-                    //Toast.makeText(AuthActivity.this, "Llenar todos los campos", Toast.LENGTH_SHORT).show();
-                }else{
-                    i = new Intent(AuthActivity.this,MainActivity.class);
-                    startActivity(i);
-                }
-            }
-        });
     }
 }
--- app/src/main/java/mx/pi5/localito/MainActivity.java
+++ app/src/main/java/mx/pi5/localito/MainActivity.java
@@ -3,6 +3,12 @@ package mx.pi5.localito;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import mx.pi5.localito.databinding.ActivityMainBinding;
 
@@ -14,6 +20,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
-
+        b.searchBar.setOnClickListener(v -> {
+            Toast.makeText(this, "A", Toast.LENGTH_SHORT).show();
+        });
+        // b.topbar.setOnMenuItemClickListener(this::navigationElement);
     }
+
+    protected boolean navigationElement(MenuItem item) {
+        if (item.getItemId() == R.id.app_search) {
+            doSearch();
+        }
+        return true;
+    }
+
+    protected void doSearch() {
+    }
+
 }
