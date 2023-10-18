c730b1369fb9804b1d7b5f913e49e7f460d1fbd4
Maskeit <miguelagustin182@gmail.com>
2023-10-17 18:42:33 -0600

    Fix:I have modified the design of Categories
    I added some vectors just for example, I also made a file for the category image buttons

--- app/src/main/java/mx/pi5/localito/layouts/categoriesView.java
+++ app/src/main/java/mx/pi5/localito/layouts/categoriesView.java
@@ -1,16 +1,17 @@
 package mx.pi5.localito.layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import mx.pi5.localito.databinding.ActivityCategoriesViewBinding;
 import android.os.Bundle;
 
 import mx.pi5.localito.R;
 
 public class categoriesView extends AppCompatActivity {
-
+    ActivityCategoriesViewBinding b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_categories_view);
+        b = ActivityCategoriesViewBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
     }
 }
