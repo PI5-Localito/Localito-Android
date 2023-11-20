814dcb42e69398801fe75c951708ab4ce4471d47
Ximenita <54044787+ximenamzo@users.noreply.github.com>
2023-11-20 03:36:48 -0600

    feat: Changes just for terms and privacy
    Discard the rest of the code for submit in other branch

--- app/src/main/java/mx/pi5/localito/auth/AnonymRegister.java
+++ app/src/main/java/mx/pi5/localito/auth/AnonymRegister.java
@@ -5,13 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.Html;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ForegroundColorSpan;
 import android.view.MenuItem;
@@ -19,23 +15,14 @@ import android.widget.CheckBox;
 import android.text.SpannableString;
 import android.text.style.ClickableSpan;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
-
-import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.textfield.TextInputEditText;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Objects;
 
 import mx.pi5.localito.R;
 import mx.pi5.localito.databinding.AnonymRegisterBinding;
@@ -44,7 +31,6 @@ public class AnonymRegister extends AppCompatActivity {
 
     AnonymRegisterBinding b;
     Intent i;
-    private boolean passwordRequirementsVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,60 +44,11 @@ public class AnonymRegister extends AppCompatActivity {
         setSupportActionBar(b.btnTopAppBack);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Botón que abre calendario para seleccionar fecha
-        b.btnCalendar.setOnClickListener(v -> {
-            MaterialDatePicker<Long> materialDatePicker = MaterialDatePicker.Builder.datePicker()
-                .setTitleText("Fecha de Nacimiento")
-                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
-                .build();
-            materialDatePicker.addOnPositiveButtonClickListener(selection -> {
-                String fecha = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date(selection));
-                b.campoFenac.setText(fecha);
-            });
-            materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER_TAG");
-        });
-
-        // Configuraciones de los campos de contraseñas
-        confPass();
-
         // Configuraciones para los Términos y Privacidad
         CheckBox ckbTerms = findViewById(R.id.ckbTerms);
         setupClickableCheckBox(ckbTerms, R.string.terms_text, R.string.terms_title, "Terms");
         CheckBox ckbPolicy = findViewById(R.id.ckbPolicy);
         setupClickableCheckBox(ckbPolicy, R.string.policy_text, R.string.policy_title, "Policy");
-
-        // TODO
-        // Botón para registrar
-        b.btnRegister.setOnClickListener(view -> {
-            String nombre, correo, telefono, fenac, contra, contra2;
-            nombre = Objects.requireNonNull(b.campoNombre.getText()).toString();
-            correo = Objects.requireNonNull(b.campoEmail.getText()).toString();
-            telefono = Objects.requireNonNull(b.campoTelCel.getText()).toString();
-            fenac = Objects.requireNonNull(b.campoFenac.getText()).toString();
-            contra = Objects.requireNonNull(b.password.getText()).toString();
-            contra2 = b.password.getText().toString();
-            if (!nombre.isEmpty() && !correo.isEmpty() && !telefono.isEmpty() && !fenac.isEmpty()){
-                if (!contra.isEmpty() || !contra2.isEmpty()) {
-                    if (contra2.equals(contra)) {
-                        if (b.ckbTerms.isChecked() && b.ckbPolicy.isChecked()) {
-                            // TODO Intent para registrar
-                            Toast.makeText(this, "Registrando...", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(this, "Debes aceptar los Términos y condiciones y la Política de Privacidad para hacer uso de la aplicación.", Toast.LENGTH_LONG).show();
-                        }
-                    } else {
-                        Toast.makeText(this, "Las contraseñas no coinciden.", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(this, "Por favor, llena ambos campos de las contraseñas.", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(this, "Por favor, completa los campos faltantes.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // Ir al Login si ya se tiene cuenta
-        b.btnToLogin.setOnClickListener(view -> { i = new Intent(this, AnonymLogin.class); startActivity(i); });
     }
 
     // Opciones del menú superior
@@ -123,123 +60,6 @@ public class AnonymRegister extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    private void confPass() {
-        TextInputEditText etxPass1 = findViewById(R.id.password);
-        TextInputEditText etxPass2 = findViewById(R.id.password2);
-        TextView requirements = findViewById(R.id.contraReq);
-        TextView passDif = findViewById(R.id.contraDif);
-
-        // Mostrar el cumplimiento de requerimientos para la contraseña
-        etxPass1.setOnFocusChangeListener((view, hasFocus) -> {
-            if (hasFocus) showPasswordRequirements();
-            else hidePasswordRequirements();
-        });
-
-        // Actualiza los requerimientos mientras se escribe la contraseña
-        etxPass1.addTextChangedListener(new TextWatcher() {
-            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-            @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-            @Override public void afterTextChanged(Editable editable) {
-                String password = editable.toString();
-                updatePasswordRequirements(requirements, password);
-            }
-        });
-
-        // Muestra texto de coincidencia de contraseñas
-        etxPass2.addTextChangedListener(new TextWatcher() {
-            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-            @Override public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-            @Override public void afterTextChanged(Editable editable) {
-                String password1 = Objects.requireNonNull(etxPass1.getText()).toString();
-                String password2 = editable.toString();
-                if (!password1.equals(password2)) {
-                    passDif.setVisibility(View.VISIBLE);
-                } else {
-                    passDif.setVisibility(View.GONE);
-                }
-            }
-        });
-    }
-
-    // Muestra requerimientos de contraseñas
-    private void showPasswordRequirements() {
-        if (!passwordRequirementsVisible) {
-            updatePasswordRequirements(b.contraReq, Objects.requireNonNull(b.password.getText()).toString());
-            passwordRequirementsVisible = true;
-        }
-    }
-
-    // Oculta requerimientos de contraseñas
-    private void hidePasswordRequirements() {
-        if (passwordRequirementsVisible) {
-            b.contraReq.setVisibility(View.GONE);
-            passwordRequirementsVisible = false;
-        }
-    }
-
-    // Función que actualiza los requerimientos
-    private void updatePasswordRequirements(TextView textView, String password) {
-        int green = ContextCompat.getColor(this, R.color.md_theme_light_secondary);
-        int red = ContextCompat.getColor(this, R.color.md_theme_dark_errorContainer);
-
-        SpannableStringBuilder builder = new SpannableStringBuilder();
-
-        if (password.length() >= 8) {
-            builder.append(getColoredSpanned("✓ Al menos 8 caracteres\n", green));
-        } else {
-            builder.append(getColoredSpanned("✗ Al menos 8 caracteres\n", red));
-        }
-
-        if (containsUppercase(password)) {
-            builder.append(getColoredSpanned("✓ Al menos una mayúscula\n", green));
-        } else {
-            builder.append(getColoredSpanned("✗ Al menos una mayúscula\n", red));
-        }
-
-        if (containsLowercase(password)) {
-            builder.append(getColoredSpanned("✓ Al menos una minúscula\n", green));
-        } else {
-            builder.append(getColoredSpanned("✗ Al menos una minúscula\n", red));
-        }
-
-        if (containsDigit(password)) {
-            builder.append(getColoredSpanned("✓ Al menos un número\n", green));
-        } else {
-            builder.append(getColoredSpanned("✗ Al menos un número\n", red));
-        }
-
-        if (containsSpecialChar(password)) {
-            builder.append(getColoredSpanned("✓ Al menos un carácter especial\n", green));
-        } else {
-            builder.append(getColoredSpanned("✗ Al menos un carácter especial\n", red));
-        }
-
-        if (!containsNumericSequence(password)) {
-            builder.append(getColoredSpanned("✓ No secuencias numéricas\n", green));
-        } else {
-            builder.append(getColoredSpanned("✗ No secuencias numéricas\n", red));
-        }
-
-        textView.setVisibility(View.VISIBLE);
-        textView.setText(builder);
-    }
-
-    // Hace el cambio de texto y estilos
-    private SpannableString getColoredSpanned(String text, int color) {
-        SpannableString spannableString = new SpannableString(text);
-        ForegroundColorSpan colorSpan = new ForegroundColorSpan(color);
-        spannableString.setSpan(colorSpan, 0, text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        return spannableString;
-    }
-
-    // Chequeo individual de requerimientos
-    private boolean containsUppercase(String str) { return str.matches(".*[A-Z].*"); }
-    private boolean containsLowercase(String str) { return str.matches(".*[a-z].*"); }
-    private boolean containsDigit(String str) { return str.matches(".*\\d.*"); }
-    private boolean containsSpecialChar(String str) { return str.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\",./<>?].*"); }
-    private boolean containsNumericSequence(String str) { return str.matches(".*123\\d*.*"); }
-
 
     // Configuración de los CheckBox, les pone texto y llama a dialogos
     private void setupClickableCheckBox(CheckBox checkBox, int textResId, int titleResId, String name) {
