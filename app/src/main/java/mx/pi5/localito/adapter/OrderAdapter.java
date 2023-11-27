9924c70418c87b082bcf871af02b693d9ea3b70c
Maskeit <miguelagustin182@gmail.com>
2023-11-27 00:29:40 -0600

    fix: basic orders desgin
    order design, order item, fragment categories

--- app/src/main/java/mx/pi5/localito/adapter/OrderAdapter.java
+++ app/src/main/java/mx/pi5/localito/adapter/OrderAdapter.java
@@ -16,11 +16,14 @@ import com.android.volley.toolbox.ImageLoader;
 import java.util.List;
 
 import mx.pi5.localito.ApiRequest;
+import mx.pi5.localito.R;
 import mx.pi5.localito.activity.ProductosActivity;
 import mx.pi5.localito.databinding.OrderItemBinding;
 import mx.pi5.localito.entity.Order;
 import mx.pi5.localito.service.Client;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
     class ViewHolder extends RecyclerView.ViewHolder {
@@ -44,12 +47,45 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
 
         public void bind(Order order) {
             this.order = order;
-            binding.orderId.setText(order.id + "");
+            Integer Id = order.id;
+            String estado =  order.state;
+            String fecha = dateFormat(order.date);
+            if(estado.contains("PENDING")){
+                estado = "Pendiente de respuesta";
+            } else if (estado.contains("ACCEPTED")) {
+                estado = "Pedido CONFIRMADO por el vendedor";
+                binding.orderIconState.setImageResource(R.drawable.baseline_confirm_24);
+            } else if (estado.contains("REJECTED")) {
+                estado = "Pedido RECHAZADO por el vendedor";
+                binding.orderIconState.setImageResource(R.drawable.baseline_rejected_24);
+            } else if (estado.contains("FINISHED")) {
+                estado = "Pedido FINALIZADO";
+                binding.orderIconState.setImageResource(R.drawable.baseline_finished_24);
+            }
+            binding.orderId.setText("Tu pedido es el núm "+ Id);
+            binding.orderState.setText(estado);
+            binding.orderCreated.setText(fecha);
+
             binding.getRoot().setOnClickListener(this::onClick);
             binding.getRoot().setVisibility(View.VISIBLE);
         }
     }
 
+    public String dateFormat(String fechaOriginal){
+        SimpleDateFormat formatoOriginal = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Date fechaDate = null;
+        try {
+            fechaDate = formatoOriginal.parse(fechaOriginal);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        SimpleDateFormat formatoDeseado = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String fechaFormateada = formatoDeseado.format(fechaDate);
+
+        return fechaFormateada;
+    }
     protected List<Order> orders;
 
     public OrderAdapter(List<Order> orders) {
@@ -77,3 +113,14 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
         return orders.size();
     }
 }
+
+//[
+//    {
+//    buyer_id: 20,
+//    seller_id: 4,
+//    stand_id: 3,
+//    date: "2023-11-27 03:11:26",
+//    state: "PENDING",
+//    id: 15,
+//    },
+//];
